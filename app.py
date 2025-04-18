3c1c7e7a8f672f1ae1c87149c7fca22e99789c01
周藤章良 <sutouakira@sutouakiraryounoMac-mini.local>
2025-04-19 00:55:41 +0900

    fix: flask run setup for Render

--- app.py
+++ app.py
@@ -76,3 +76,5 @@ def handle_message(event):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+    if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
